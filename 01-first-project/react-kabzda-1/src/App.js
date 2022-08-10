a0cf1641a323edeed3a7b3b3bfa666ed95f30cd5
Artur_Barilo <bariloartur@gmail.com>
2022-08-10 16:29:51 +0300

    lesson 6(index.js, App, JSX)

---
+++ 01-first-project/react-kabzda-1/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <div>Another DIV</div>
+    <div className="App">   
+      <ul>
+        <li>sdfgsdg</li>
+        <li>sdfgs</li>
+        <li>sdfdf</li>
+      </ul>
+    </div>
+    </div>
+  );
+}
+
+export default App;
