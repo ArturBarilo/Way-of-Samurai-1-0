4aefef80b256b9652acb5435a59f4fc19db0f18c
Artur_Barilo <bariloartur@gmail.com>
2022-08-16 18:14:29 +0300

    lesson 12(breakdown into components)

--- 01-first-project/react-kabzda-1/src/App.js
+++ 01-first-project/react-kabzda-1/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 
 
@@ -7,49 +10,9 @@ import './App.css';
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src='https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn' />
-      </header>
-      <nav className='nav'>
-        <div>
-          <a href=''>Profile</a>
-        </div>
-        <div>
-          <a href=''>Mesages</a>
-        </div>
-        <div>
-          <a href=''>News</a>
-        </div>
-        <div>
-          <a href=''>Music</a>
-        </div>
-        <div>
-          <a href=''>Settings</a>
-        </div>
-      </nav>
-
-      <div className='content'>
-        <div>
-          <img src='https://media-exp1.licdn.com/dms/image/C4D1BAQGDmALg_8s-Yg/company-background_10000/0/1519799119530?e=2147483647&v=beta&t=MpzHeo7wdMoePy-CjWNPwwMbgDU3ydtdqIXGYFtSisg'/>
-        </div>
-        <div>
-          ava + description
-        </div>
-        <div>
-          My posts
-          <div>
-            New post
-          </div>
-          <div>
-            <div>
-              post 1
-            </div>
-            <div>
-              post 2
-            </div>
-          </div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ 01-first-project/react-kabzda-1/src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://media-exp1.licdn.com/dms/image/C4D1BAQGDmALg_8s-Yg/company-background_10000/0/1519799119530?e=2147483647&v=beta&t=MpzHeo7wdMoePy-CjWNPwwMbgDU3ydtdqIXGYFtSisg' />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
