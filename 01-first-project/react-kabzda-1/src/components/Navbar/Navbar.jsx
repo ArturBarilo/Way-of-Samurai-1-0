092a22f1d2f8b1f98619c5e0fd742102989ab93f
Artur_Barilo <bariloartur@gmail.com>
2022-09-01 17:10:11 +0300

    lesson20/2 (Corrected active Navlink)

--- 01-first-project/react-kabzda-1/src/components/Navbar/Navbar.jsx
+++ 01-first-project/react-kabzda-1/src/components/Navbar/Navbar.jsx
@@ -5,19 +5,19 @@ const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
+                <NavLink to="/profile" className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/dialogs">Mesages</NavLink>
+                <NavLink to="/dialogs" className = { navData => navData.isActive ? s.active : s.item }>Mesages</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/news">News</NavLink>
+                <NavLink to="/news" className = { navData => navData.isActive ? s.active : s.item }>News</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/music">Music</NavLink>
+                <NavLink to="/music" className = { navData => navData.isActive ? s.active : s.item }>Music</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/settings">Settings</NavLink>
+                <NavLink to="/settings" className = { navData => navData.isActive ? s.active : s.item }>Settings</NavLink>
             </div>
         </nav>
     )
